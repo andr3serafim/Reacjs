c9f04122149e80f79e48e0feedcd40f8af2dff5d
lusacann <contato.andremoreira@gmail.com>
2023-01-11 15:24:48 -0300

    update

---
+++ organo/src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return(
+        <header className='banner'>
+            <img src="./img/Banner.png" alt="O banner principald da página"/>
+        </header>
+    )
+}
+
+export default Banner
